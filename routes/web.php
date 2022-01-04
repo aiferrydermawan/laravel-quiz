15fdcecb6fef5b2c8d32500f315b42ec3559fbc6
aiferrydermawan <aiferrydermawan@gmail.com>
2022-01-04 18:41:17 +0700

    langsung redirect ke quiz

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('questions.index');
 });
 
 Route::get('/questions',QuestionController::class)->name('questions.index');
